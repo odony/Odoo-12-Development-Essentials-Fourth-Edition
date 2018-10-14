2f694412dc17f4b7e7845ad431d5e5ffd3276006
Daniel Reis <dreis.pt@hotmail.com>
2018-10-14 23:07:40 +0100

    ch04

---
+++ ch04/library_member/__manifest__.py
@@ -0,0 +1,14 @@
+{ 
+'name': 'Library Members and Borrowing', 
+'description': 'Use library cards for people to borrow books.', 
+'author': 'Daniel Reis', 
+'data': [
+    'views/book_view.xml',
+],
+'depends': [
+    'library_app',
+    'mail',
+], 
+'application': False, 
+} 
+

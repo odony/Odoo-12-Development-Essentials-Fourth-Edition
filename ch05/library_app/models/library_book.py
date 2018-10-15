c68aa51d0e47601d961aca088bbe31995062806f
Daniel Reis <dgreis@sapo.pt>
2018-10-15 14:47:08 +0100

    ch05 fix

--- ch05/library_app/models/library_book.py
+++ ch05/library_app/models/library_book.py
@@ -1,16 +1,5 @@
-from odoo import api, fields, models
+from odoo import fields, models
 from odoo.exceptions import Warning
-
-
-# Ch03
-def check_isbn(isbn):
-    digits = [int(x) for x in isbn if x.isdigit()]
-    if len(isbn)==13:
-        ponderators = [1, 3] * 6
-        total = sum(a * b for a,b in zip(digits[:12], ponderators))
-        remain = total % 10
-        check = 10 - remain if remain != 0 else 0
-        return digits[-1] == check
 
 
 class Book(models.Model):
@@ -25,12 +14,22 @@ class Book(models.Model):
     publisher_id = fields.Many2one('res.partner', string='Publisher')
     author_ids = fields.Many2many('res.partner', string='Authors')
 
-    @api.multi
+    def _check_isbn(self):
+        """Check one Book's ISBN"""
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 13:
+            ponderators = [1, 3] * 6
+            total = sum(a * b for a, b in zip(digits[:12], ponderators))
+            remain = total % 10
+            check = 10 - remain if remain != 0 else 0
+            return digits[-1] == check
+
     def button_check_isbn(self):
         for book in self:
             if not book.isbn:
-               raise Warning(
-                  'Please provide an ISBN13 for %s' % book.name) 
-            if book.isbn and not check_isbn(book.isbn):
-               raise Warning(
-                  '%s is an invalid ISBN' % book.isbn) 
+                raise Warning(
+                    'Please provide an ISBN13 for %s' % book.name)
+            if book.isbn and not book._check_isbn():
+                raise Warning(
+                    '%s is an invalid ISBN' % book.isbn)

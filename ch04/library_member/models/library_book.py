154602a516651bf1ce315663bc5bba8082f5bbe3
Daniel Reis <dgreis@sapo.pt>
2018-10-15 15:35:27 +0100

    ch04 wip

--- ch04/library_member/models/library_book.py
+++ ch04/library_member/models/library_book.py
@@ -1,5 +1,18 @@
 from odoo import api, fields, models
- 
-class Book(models.Model): 
-    _inherit = 'library.book' 
+
+
+class Book(models.Model):
+    _inherit = 'library.book'
     is_available = fields.Boolean('Is Available?', readonly=False)
+
+    @api.multi
+    def _check_isbn(self):
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 10:
+            ponderators = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            total = sum(a * b for a, b in zip(digits[:9], ponderators))
+            check = total % 11
+            return digits[-1] == check
+        else:
+            return super()._check_isbn()
--- ch04/library_member/models/library_member.py
+++ ch04/library_member/models/library_member.py
@@ -1,9 +1,10 @@
 from odoo import fields, models
 
-class MemberCard(models.Model): 
+
+class Member(models.Model):
     _name = 'library.member'
     _description = 'Library Member'
-    # _inherits = {'res.partner': 'partner_id'} 
+    # _inherits = {'res.partner': 'partner_id'}
     card_number = fields.Char()
     partner_id = fields.Many2one(
-        'res.partner', delegate=True, ondelete='cascade')
+        'res.partner', delegate=True, ondelete='cascade', required=True)

5c4e5d9c6776808b97e2e6b8c40e1a4652328453
Daniel Reis <dgreis@sapo.pt>
2018-10-19 18:57:29 +0100

    ch04 wip

--- ch04/library_member/models/library_book.py
+++ ch04/library_member/models/library_book.py
@@ -5,6 +5,9 @@ class Book(models.Model):
     _inherit = 'library.book'
     is_available = fields.Boolean('Is Available?', readonly=False)
 
+    isbn = fields.Char(help="Use a valid ISBN-13 or ISBN-10.")
+    publisher_id = fields.Many2one(index=True)
+
     @api.multi
     def _check_isbn(self):
         self.ensure_one()

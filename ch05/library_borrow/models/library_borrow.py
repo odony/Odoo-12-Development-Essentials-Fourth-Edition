f074b08175c883a8fa78c0f069771fe920584e6d
Daniel Reis <dreis.pt@hotmail.com>
2018-10-21 21:53:11 +0100

    ch05 wip

--- ch05/library_borrow/models/library_borrow.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class Borrow(models.Model):
-    _name = 'library.borrow'
-    _description = 'Book Borrowing'
-   book_id = fields.Many2one('library.book', name='Book')
-   member_id = fields.Many2one('library.member', name='Member')
-
--- ch05/library_checkout/models/library_checkout_stage.py
+++ ch05/library_checkout/models/library_checkout_stage.py
@@ -2,8 +2,8 @@ from odoo import fields, models
 
 
 class Stage(models.Model):
-    _name = 'library.borrow.stage'
-    _description = 'Borrowing Stage'
+    _name = 'library.checkout.stage'
+    _description = 'Checkout Stage'
     name = fields.Char(required=True, translate=True)
     sequence = fields.Integer()
     state = fields.Selection(
@@ -12,4 +12,4 @@ class Stage(models.Model):
          ('done', 'Returned'),
          ('lost', 'Lost'),
          ('cancel', 'Cancelled')],
-        'State'])
+        'State')
